af1953ec8705414bd437496c600ad850f64ba14b
himasnhu1 <himanshusaini712@gmail.com>
2026-05-15 15:43:44 +0530

    DONE

---
+++ README.md
@@ -0,0 +1,23 @@
+# Authentication API
+
+A Node.js authentication system using JWT Bearer Token.
+
+## Features
+
+- User Registration
+- User Login
+- JWT Authentication
+- Protected Routes
+
+## Tech Stack
+
+- Node.js
+- Express.js
+- MongoDB
+- Mongoose
+- JWT
+
+## Run Project
+
+npm install
+npm run dev
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./db");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", require("../routes/authRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Auth API Running");
