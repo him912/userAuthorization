c042bb5c5200a29a9761971a2db6afdd558fac35
himasnhu1 <himanshusaini712@gmail.com>
2026-05-27 23:18:17 +0530

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -12,6 +13,13 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3005", "http://localhost:3000"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Auth API Running");
